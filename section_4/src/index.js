b2c3d6654ecddc4a5ac339956f1a4ca4817b147a
roman.shutsman@Blackthorn-vision.com <roman.shutsman@Blackthorn-vision.com>
2019-07-16 11:00:22 +0300

    section 5

--- section_4/src/index.js
+++ section_4/src/index.js
@@ -3,13 +3,38 @@ import ReactDOM from 'react-dom';
 // import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
+  constructor(props) {
+    super(props);
 
-    return <div>Lat: </div>;
+    this.state = {
+      lat: null,
+      lon: null,
+      err: ''
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        });
+      },
+      err =>
+        this.setState({
+          err: err.message
+        })
+    );
+  }
+
+  render() {
+    return this.state.lat && this.state.lon ? (
+      <div>
+        <div>Lat: {this.state.lat}</div>
+        <div>Lon: {this.state.lon}</div>
+      </div>
+    ) : (
+      <div>{this.state.err}</div>
+    );
   }
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
