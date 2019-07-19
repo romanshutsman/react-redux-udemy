069003fbc2e103dcfe1c8730ce0f9e164c81bffb
roman.shutsman@Blackthorn-vision.com <roman.shutsman@Blackthorn-vision.com>
2019-07-19 11:15:04 +0300

    add section 7

---
+++ section_7/src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="">Image Search</label>
+            <input
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
