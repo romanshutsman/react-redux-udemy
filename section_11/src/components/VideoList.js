4804e57dcbcb33a3bee21174516dbc49b78813a4
roman.shutsman@Blackthorn-vision.com <roman.shutsman@Blackthorn-vision.com>
2019-07-26 10:59:13 +0300

    section 11: implemented video player

--- section_11/src/components/VideoList.js
+++ section_11/src/components/VideoList.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
-  return <div className="ui segment">{props.videos.length}</div>;
+const VideoList = ({ videos, onVideoSelect }) => {
+  return videos.map((video, key) => (
+    <div key={key} className="ui relaxed divided list">
+      <VideoItem onVideoSelect={onVideoSelect} video={video} />
+    </div>
+  ));
 };
 export default VideoList;
