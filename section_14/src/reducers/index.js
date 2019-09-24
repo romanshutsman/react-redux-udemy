fccf346920c42fe489b43b246feebbfef0bf9648
roman.shutsman@Blackthorn-vision.com <roman.shutsman@Blackthorn-vision.com>
2019-09-24 12:43:36 +0300

    section 15 redux store design

--- section_14/src/reducers/index.js
+++ section_14/src/reducers/index.js
@@ -1,28 +1,9 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
-// const songReducer = () => {
-//   return [
-//     {
-//       title: 'No scrubs',
-//       duration: '4:05'
-//     },
-//     {
-//       title: 'All star',
-//       duration: '3:14'
-//     },
-//     {
-//       title: 'Macarena',
-//       duration: '2:30'
-//     }
-//   ];
-// };
-// const selectedSongReducer = (selectedSong = null, action) => {
-//   if (action.type === 'SONG_SELECTED') {
-//     return action.payload;
-//   }
-//   return selectedSong;
-// };
 
 export default combineReducers({
-    replaceMe: () => 'dummy'
+    posts: postsReducer,
+    users: usersReducer
 });
